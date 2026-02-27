9c990e43885f0fdd046fc0c0d62e437a9e02730c
YudoLally <yudhvir.lally@gmail.com>
2026-02-27 21:35:15 +1100

    feat: finished task 2

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 // ==== Type Definitions, feel free to add or modify ==========================
 interface cookbookEntry {
   name: string;
-  type: string;
+  type: "recipe" | "ingredient";
 }
 
 interface requiredItem {
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: cookbookEntry[] = [];
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -39,7 +39,6 @@ app.post("/parse", (req:Request, res:Response) => {
   } 
   res.json({ msg: parsed_string });
   return;
-  
 });
 
 // [TASK 1] ====================================================================
@@ -58,10 +57,52 @@ const parse_handwriting = (recipeName: string): string | null => {
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
-
+  const input = req.body as cookbookEntry;
+  console.log("calling routeMEOWMEOWMEOW");
+  const result = createEntry(input)
+  if (result == false) {
+    return res.status(400).send();
+  } 
+  return res.status(200).send();
 });
+
+function createEntry(entry: cookbookEntry): Boolean {
+  console.log("calling functionMEOWMEOWMEOW");
+  if (entry.type !== "recipe" && entry.type !== "ingredient") {
+    console.log("fail1");
+    return false;
+  }
+
+  if (cookbook.some(e => e.name === entry.name)) {
+    console.log("fail2");
+    return false;
+  }
+
+  if (entry.type === "ingredient") {
+    const entryIngred = entry as ingredient;
+    if (entryIngred.cookTime === undefined || entryIngred.cookTime < 0) {
+      console.log("fail3");
+      return false;
+    }
+  }
+
+  if (entry.type === "recipe") {
+    const entryRecipe = entry as recipe;
+    if (entryRecipe.requiredItems) {
+      const names = entryRecipe.requiredItems.map(reqItem => reqItem.name);
+
+      if (new Set(names).size !== names.length) {
+        console.log("fail4");
+        return false;
+      }
+    }
+  }
+
+  cookbook.push(entry);
+  console.log("SUCCESSFULLY ADDED");
+  console.log(cookbook);
+  return true;
+} 
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
